64793a7e1f2553f5f7be9cd143f970da8cf2f26b
Abdullah <ab-benkathir@outlook.com>
2021-09-16 11:25:35 +0200

    nieuwe code

--- de lab F1.3.01.L2 - Kaas, kaas en nog eens kaas!/wieIsHet.py
+++ de lab F1.3.01.L2 - Kaas, kaas en nog eens kaas!/wieIsHet.py
@@ -1,43 +1,37 @@
-print("Is de kaas geel?")
-a = input("Ja/Nee")
-if a ==  "ja":
-    print("Zitten er gaten in?")
-    b = input("ja/nee")
-    if b == "ja":
-        print("Is de kaas belachelijk duur")
-        c = input("ja/nee")
-        if c == "ja":
-            print("Emmenthaler")
-        else :
-                 print("Leerdammer")
-    else:
-       if b == "nee":
-           print("Is de kaas hard als steen?")
-           d = input("ja/nee")
-           if d == "ja":
-               print("Parmigiano Reggiano")
-
-           else:
-                if d == "nee":
+while True:    
+    print("Is de kaas geel?")
+    a = input("Ja/Nee")
+    if a ==  "ja":
+        print("Zitten er gaten in?")
+        b = input("ja/nee")
+        if b == "ja":
+            print("Is de kaas belachelijk duur")
+            c = input("ja/nee")
+            if c == "ja":
+                print("Emmenthaler")
+            elif c == "nee":
+                    print("Leerdammer")
+        elif  b == "nee":
+                print("Is de kaas hard als steen?")
+                d = input("ja/nee")
+                if d == "ja":
+                    print("Parmigiano Reggiano")
+                elif d == "nee":
                     print("Gaudse kaas")
-else:
-    if a == "nee":
-        print("Heeft de kaas blauwe schimmels?")
-        e = input("ja/nee")
-        if e == "ja":
-            print("Heeft de kaas een korst?")
-            f = input("ja/nee")
-            if f == "ja":
-                print("Bleu de Rochbaron")
-            else:
-                if f == "nee":
-                    print("Foume D'Ambert")  
-        else:
-            if e == "nee":
+    elif a == "nee":
+            print("Heeft de kaas blauwe schimmels?")
+            e = input("ja/nee")
+            if e == "ja":
                 print("Heeft de kaas een korst?")
-                g = input("ja/nee")
-                if g == "ja":
-                    print("Camembert")
-                else:
-                    if g == "nee":
-                        print("Mozzarella")    
+                f = input("ja/nee")
+                if f == "ja":
+                    print("Bleu de Rochbaron")
+                elif f == "nee":
+                        print("Foume D'Ambert")  
+            elif e == "nee":
+                    print("Heeft de kaas een korst?")
+                    g = input("ja/nee")
+                    if g == "ja":
+                        print("Camembert")
+                    elif g == "nee":
+                            print("Mozzarella")            
